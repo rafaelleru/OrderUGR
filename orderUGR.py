e1ff16e19417702c84831e11829baa2575f4b920
rafaelleru <rafaelleru95103@gmail.com>
2016-03-07 14:57:46 +0100

    ahora funciona para todos los cursos de ingenieria informatica,\n añadida nueva funcion que renombra archivos

--- orderUGR.py
+++ orderUGR.py
@@ -1,3 +1,4 @@
+#!/bin/usr/python3
 import sys
 import os
 
@@ -5,7 +6,27 @@ home = os.path.expanduser("~")
 Descargas = os.path.join(home, "Descargas")
 UGR = os.path.join(home, "UGR")
 
-def compruebaArchivo(cadena, archivo):
+primero = os.path.join(UGR, "primero")
+segundo = os.path.join(UGR, "segundo")
+tercero = os.path.join(UGR, "tercero")
+cuarto = os.path.join(UGR, "cuarto")
+
+curso1 = [
+    'CA', 'ALEM', 'FS', 'FP', 'FFT'
+    'LMD', 'TOC', 'MP', 'ES', 'IES']
+
+curso2 = [
+    'SO', 'SCD', 'EC', 'ED', 'PDOO',
+    'IA', 'FIS', 'AC', 'ALG', 'FBD']
+
+curso3 = [
+    'MC', 'IG', 'DDSI', 'FR', 'ISE',
+    ] #Mas cosas
+
+cursos = [ curso1, curso2, curso3 ]
+path_cursos = [primero, segundo, tercero, cuarto]
+
+def eliminaCadena(cadena, archivo):
     archivo_nuevo = ""
 
     if "_"+cadena in archivo:
@@ -15,20 +36,27 @@ def compruebaArchivo(cadena, archivo):
     else:
         archivo_nuevo = archivo
 
-    print(archivo_nuevo)
     return archivo_nuevo
 
+
+def renombraPath(archivo):
+    nuevo_path = None
+    for curso in cursos:
+        for asignatura in curso:
+            if asignatura in archivo:
+                ncurso = cursos.index(curso)
+                archivo_nuevo = eliminaCadena(asignatura, archivo)
+                nuevo_path = os.path.join(path_cursos[ncurso], os.path.join(asignatura, archivo_nuevo))
+    return nuevo_path
+
 def mueveArchivos():
-    tipoDeArchivos = [
-        'TSI', 'FBD', 'IC',
-        'IA', 'AC', 'ALG'
-        ]
+
     for path, dirs, files in os.walk(Descargas):
         for arch in files:
-            for tipo in tipoDeArchivos:
-                if tipo in arch:
-                    arch_nuevo=compruebaArchivo(tipo, arch)
-                    os.rename(os.path.join(Descargas,arch), os.path.join(UGR, tipo + "/" + arch_nuevo))   
+            nuevo_archivo = renombraPath(arch)
+            if not (nuevo_archivo is None):
+                os.rename(os.path.join(Descargas, arch), nuevo_archivo)
+                print(nuevo_archivo)
 
 
 mueveArchivos()
